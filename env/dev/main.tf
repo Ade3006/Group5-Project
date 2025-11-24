f2f5bf45493c0d904273e78f001d1c53dc38815c
aromolaran <aaromolaran1@myseneca.ca>
2025-11-24 16:32:43 +0000

    Added Terraform multi-environment structure

---
+++ env/dev/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  backend "s3" {
+    bucket = "my-dev-env-bucket05"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "my-network-state-bucket05"
+    key    = "network/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+module "dev" {
+  source = "../../modules/env"
+
+  env_name        = "dev"
+  instance_type   = "t3.micro"
+  asg_min_size    = 2
+  asg_max_size    = 4
+  bucket_name     = "my-dev-env-bucket05"
+  images_prefix   = "images/"
+
+  key_name          = "vockey"
+  private_subnet_ids = data.terraform_remote_state.network.outputs.private_subnet_ids
+  web_sg_id          = data.terraform_remote_state.network.outputs.web_sg_id
+  target_group_arn   = data.terraform_remote_state.network.outputs.tg_dev_arn
+}
